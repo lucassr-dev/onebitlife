4145b1a0397a9710fd8001d261f1368b8ace8788
Lucasdev <lucas-cs-2008@hotmail.com>
2022-12-01 23:45:46 -0300

    Create Habit e estrutura de rotas

--- src/Components/Home/CreateHabit/index.jsx
+++ src/Components/Home/CreateHabit/index.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export default function CreateHabit({ habitArea, borderColor }) {
+  const navigation = useNavigation()
+
   function handleCreate() {
-    console.log(`Botão da área clicado: ${habitArea}`);
+    navigation.navigate('HabitPage', {
+      create: true,
+      habit: { habitArea: habitArea }
+    })
   }
 
   return (
@@ -13,10 +19,10 @@ export default function CreateHabit({ habitArea, borderColor }) {
       onPress={handleCreate}
     >
       <Text style={styles.habitTitle}>
-        Adicionar meta {habitArea === "Mente" ? "da" : "do"} {habitArea}
+        Adicionar meta {habitArea === 'Mente' ? 'da' : 'do'} {habitArea}
       </Text>
     </TouchableOpacity>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -26,15 +32,15 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderWidth: 2,
-    borderStyle: "dotted",
-    borderColor: "white",
+    borderStyle: 'dotted',
+    borderColor: 'white',
     borderRadius: 5,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   habitTitle: {
-    color: "white",
+    color: 'white',
     fontSize: 16,
-    fontWeight: "bold",
-  },
-});
+    fontWeight: 'bold'
+  }
+})
